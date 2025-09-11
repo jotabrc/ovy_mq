a0afef49b2f96c9804c9ee9feececc2b7d99be23
jotabrc <ojcroveda@gmail.com>
2025-09-11 20:56:07 -0300

    initial commit

---
+++ src/main/java/io/github/jotabrc/OVY_Mq/OvyMqApplication.java
@@ -0,0 +1,13 @@
+package io.github.jotabrc.OVY_Mq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OvyMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OvyMqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/jotabrc/OVY_Mq/OvyMqApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jotabrc.OVY_Mq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OvyMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
