2db943dbb882c40ff43daca81792750ed82c2f96
jotabrc <ojcroveda@gmail.com>
2025-11-18 21:28:36 -0300

    refact: listener initializer

--- ovy_mq_client/src/main/java/io/github/jotabrc/ovy_mq_client/component/initialize/ClientListenerInitializer.java
+++ ovy_mq_client/src/main/java/io/github/jotabrc/ovy_mq_client/component/initialize/ClientListenerInitializer.java
@@ -46,50 +46,64 @@ public class ClientListenerInitializer implements BeanPostProcessor {
 
             for (Method method : beanClass.getMethods()) {
                 OvyListener listener = AnnotationUtils.findAnnotation(method, OvyListener.class);
-
                 if (nonNull(listener)) {
                     log.info("Listener: topic={} quantity={}", listener.topic(), listener.quantity());
                     AtomicInteger i = new AtomicInteger(1);
                     while (i.getAndIncrement() <= listener.quantity()) {
-                        DefinitionMap definition = definitionProvider.getObject()
-                                .add(Key.HEADER_TOPIC, listener.topic())
-                                .add(Key.FACTORY_CLIENT_METHOD, method)
-                                .add(Key.FACTORY_CLIENT_BEAN_NAME, beanName)
-                                .add(Key.FACTORY_CLIENT_TIMEOUT, listener.timeout())
-                                .add(Key.HEADER_CLIENT_TYPE, ClientType.CONSUMER)
-                                .add(Key.FACTORY_CLIENT_IS_AVAILABLE, true);
-                        factoryResolver.create(definition, Client.class)
-                                .ifPresent(client -> {
-                                    log.info("Creating client: replica={}/{} topic={} replica-config=[quantity={} max={} min={} step={} autoManage={} timeout={}ms]",
-                                            i.get() + 1,
-                                            listener.quantity(),
-                                            listener.topic(),
-                                            listener.quantity(),
-                                            listener.max(),
-                                            listener.min(),
-                                            listener.step(),
-                                            listener.autoManage(),
-                                            listener.timeout());
-                                    DefinitionMap sessionDefinition = definitionProvider.getObject()
-                                            .add(Key.FACTORY_CLIENT_OBJECT, client)
-                                            .add(Key.FACTORY_SUBSCRIPTIONS, Subscribe.CONSUMER_SUBSCRIPTION.apply(client.getTopic()));
-                                    sessionInitializer.createSessionAndConnect(client, sessionDefinition);
-                                    clientRegistry.save(client);
-                                });
+                        createClient(beanName, method, listener, i);
                     }
-
-                    DefinitionMap definition = definitionProvider.getObject()
-                            .add(Key.HEADER_TOPIC, Value.ROLE_SERVER)
-                            .add(Key.FACTORY_CLIENT_TIMEOUT, listener.timeout())
-                            .add(Key.FACTORY_REPLICA_QUANTITY, listener.quantity())
-                            .add(Key.FACTORY_REPLICA_MAX, listener.max())
-                            .add(Key.FACTORY_REPLICA_MIN, listener.min())
-                            .add(Key.FACTORY_REPLICA_STEP, listener.step())
-                            .add(Key.FACTORY_REPLICA_AUTO_MANAGE, listener.autoManage());
-                    factoryResolver.create(definition, ListenerConfig.class).ifPresent(listenerConfigRegistry::save);
+                    createListenerConfig(listener);
                 }
             }
         });
         return BeanPostProcessor.super.postProcessAfterInitialization(bean, beanName);
+    }
+
+    private void createClient(String beanName, Method method, OvyListener listener, AtomicInteger i) {
+        DefinitionMap definition = definitionProvider.getObject()
+                .add(Key.HEADER_TOPIC, listener.topic())
+                .add(Key.FACTORY_CLIENT_METHOD, method)
+                .add(Key.FACTORY_CLIENT_BEAN_NAME, beanName)
+                .add(Key.FACTORY_CLIENT_TIMEOUT, listener.timeout())
+                .add(Key.HEADER_CLIENT_TYPE, ClientType.CONSUMER)
+                .add(Key.FACTORY_CLIENT_IS_AVAILABLE, true);
+        factoryResolver.create(definition, Client.class)
+                .ifPresent(client -> {
+                    logClientCreation(i, listener);
+                    createClientSessionManager(client);
+                    clientRegistry.save(client);
+                });
+    }
+
+    private void logClientCreation(AtomicInteger i, OvyListener listener) {
+        log.info("Creating client: replica={}/{} topic={} replica-config=[quantity={} max={} min={} step={} autoManage={} timeout={}ms]",
+                i.get() + 1,
+                listener.quantity(),
+                listener.topic(),
+                listener.quantity(),
+                listener.max(),
+                listener.min(),
+                listener.step(),
+                listener.autoManage(),
+                listener.timeout());
+    }
+
+    private void createClientSessionManager(Client client) {
+        DefinitionMap sessionDefinition = definitionProvider.getObject()
+                .add(Key.FACTORY_CLIENT_OBJECT, client)
+                .add(Key.FACTORY_SUBSCRIPTIONS, Subscribe.CONSUMER_SUBSCRIPTION.apply(client.getTopic()));
+        sessionInitializer.createSessionAndConnect(client, sessionDefinition);
+    }
+
+    private void createListenerConfig(OvyListener listener) {
+        DefinitionMap definition = definitionProvider.getObject()
+                .add(Key.HEADER_TOPIC, Value.ROLE_SERVER)
+                .add(Key.FACTORY_CLIENT_TIMEOUT, listener.timeout())
+                .add(Key.FACTORY_REPLICA_QUANTITY, listener.quantity())
+                .add(Key.FACTORY_REPLICA_MAX, listener.max())
+                .add(Key.FACTORY_REPLICA_MIN, listener.min())
+                .add(Key.FACTORY_REPLICA_STEP, listener.step())
+                .add(Key.FACTORY_REPLICA_AUTO_MANAGE, listener.autoManage());
+        factoryResolver.create(definition, ListenerConfig.class).ifPresent(listenerConfigRegistry::save);
     }
 }
